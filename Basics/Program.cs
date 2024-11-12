5d358368c017ac252a24fc7e9f8b04b1bda346ce
vaibhavnagwani <nagwanivaibhav02@gmail.com>
2024-11-12 19:05:31 +0100

    Added the project Basics

---
+++ Basics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(getMax(1, 4, 3));
+           
+        }
+
+        static int getMax(int num1, int num2, int num3)
+        {
+            int res;
+            if(num1>num2 && num1 > num3)
+            {
+                res = num1;
+            }
+            else if (num2 > num3)
+            {
+                res = num2;
+            } else
+            {
+                res = num3;
+            }
+            return res;
+        }
+    }
+}
