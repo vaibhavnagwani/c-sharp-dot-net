f8e94bafb135c66ecb9a8f5c525b9c8bb244b293
vaibhavnagwani <nagwanivaibhav02@gmail.com>
2024-11-12 19:40:41 +0100

    Added a basic calculator method to the code file

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -6,9 +6,15 @@ namespace Basics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(getMax(1, 4, 3));
-           
+            //Console.WriteLine(getMax(1, 4, 3));
+
+            Console.WriteLine("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Select the number for the operation that you want to perform: \n1. Add\n2. Subtract\n3. Multiply\n4. Divide");
+            int caseParam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nAnswer = " + calculator(num1, num2, caseParam));
         }
 
         static int getMax(int num1, int num2, int num3)
@@ -26,6 +32,29 @@ namespace Basics
                 res = num3;
             }
             return res;
+        }
+
+        static string calculator(double num1, double num2, int caseParam)
+        {
+
+            switch (caseParam)
+            {
+                case 1:
+                    return num1 + num2 + "";
+
+                case 2:
+                    return num1 - num2 + "";
+
+                case 3:
+                    return num1 * num2 + "";
+
+                case 4:
+                    if (num2 == 0) Console.WriteLine("Cannot divide by 0!");
+                    return (num1 / num2) + "";
+
+                default:
+                    return "Please enter the desired operation you want, correctly!";
+            }
         }
     }
 }
